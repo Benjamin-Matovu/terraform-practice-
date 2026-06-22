268969486426e69a13b9eaefabc503810a4c4643
Benjamin-Matovu <mbenjamina0000@gmail.com>
2026-06-22 12:40:10 +0300

    Add environment variable and update S3 bucket name
    Added a variable for environment and updated S3 bucket name.

--- main.tf
+++ main.tf
@@ -11,6 +11,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "Environment name"
+}
+
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "daddy-terraform-practice-${formatdate("YYYYMMDDhhmmss", timestamp())}"
+  bucket = "daddy-app-${var.environment}-bucket"
+
+  tags = {
+    Name        = "My App Bucket"
+    Environment = var.environment
+  }
+}
+
+output "bucket_name" {
+  value       = aws_s3_bucket.my_bucket.bucket
+  description = "The name of the S3 bucket"
 }
